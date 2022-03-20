54dd0b9833f28d9bb7a131a757744567e9bf5591
Ivan Sedykh <ivan.sedykh@deleteagency.com>
2022-03-20 13:57:22 +0500

    removing redundant configs

--- user-config.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-const { makeConfig } = require('./project-config.js');
-module.exports = {
-    default: makeConfig({ path: path.resolve('../../formulae.local'), appPool: 'FormulaE' }),
-};
